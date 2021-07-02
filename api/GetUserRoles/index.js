0f8680ba7bb826ebaf238e4688f776ed83680de9
Joy <t-joytijani@microsoft.com>
2021-07-02 14:03:42 -0400

    GetUserRoles update

--- api/GetUserRoles/index.js
+++ api/GetUserRoles/index.js
@@ -1,5 +1,14 @@
 module.exports = async function (context, req) {
+    const queryObject = url.parse(req.url,true).query;
+    const rolesArray  = "";
+    if (queryObject.userId == '1234' && queryObject.provider == 'google') {
+        rolesArray.append("authenticated, reader");
+    } else if (queryObject.userId == '1235' && queryObject.provider == 'google') {
+        rolesArray.append("contributor");
+    } else {
+        rolesArray.append();
+    }
     context.res.json({
-        roles: "authenticated, reader"
+        roles: rolesArray
     });
 };
