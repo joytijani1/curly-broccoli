8c08aa46c73d6b6aeafd3957847305ac21fc6926
joytijani1 <85193894+joytijani1@users.noreply.github.com>
2021-07-09 10:35:35 -0400

    Update index.js

--- api/GetUserRoles/index.js
+++ api/GetUserRoles/index.js
@@ -2,11 +2,11 @@ const url = require('url');
 
 module.exports = async function (context, req) {
     const queryObject = url.parse(req.url,true).query;
-    var rolesArray  = [];
+    var rolesArray  = "";
     if (queryObject.userId == '1234' && queryObject.provider == 'google') {
-        rolesArray.push("reader");
+        rolesArray = "reader";
     } else if (queryObject.userId == '1235' && queryObject.provider == 'google') {
-        rolesArray.push("contributor");
+        rolesArray = "contributor";
     } else {
         rolesArray = rolesArray;
     }
