7732a71533303e74102f24ef559ec326eb983bf9
Joy <t-joytijani@microsoft.com>
2021-07-19 14:41:53 -0400

    api update

--- api/GetUserRoles/index.js
+++ api/GetUserRoles/index.js
@@ -1,15 +1,20 @@
 const url = require('url');
 
 module.exports = async function (context, req) {
-    const queryObject = url.parse(req.url,true).query;
+    /*const queryObject = url.parse(req.url,true).query;*/
+    const header = req.headers['x-ms-client-principal'];
+    const encoded = Buffer.from(header, 'base64');
+    const decoded = encoded.toString('ascii');
+    const clientPrincipal = JSON.parse(decoded);
     var rolesArray  = [];
-    if (queryObject.userId == '1234' && queryObject.provider == 'google') {
+    if (clientPrincipal.userId == 'd6b9de92ebba49a48e4888c54a3ee8ac' && clientPrincipal.provider == 'google') {
         rolesArray.push("reader");
-    } else if (queryObject.userId == '1235' && queryObject.provider == 'google') {
+    } else if (clientPrincipal.userId == '1235' && clientPrincipal.provider == 'google') {
         rolesArray.push("contributor");
     } else {
         rolesArray = rolesArray;
     }
+
     context.res.json({
         "userRoles": rolesArray,
         "denyAccess": false
