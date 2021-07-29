3bd61273580bf16a71579b63c5236853cd1c3f38
joytijani1 <85193894+joytijani1@users.noreply.github.com>
2021-07-29 16:59:38 -0400

    update api with Token

--- api/GetUserRoles/index.js
+++ api/GetUserRoles/index.js
@@ -1,4 +1,5 @@
 //const url = require('url');
+const fetch = require("node-fetch");
 
 module.exports = async function (context, req) {
     /*const queryObject = url.parse(req.url,true).query;*/
@@ -6,18 +7,27 @@ module.exports = async function (context, req) {
     const encoded = Buffer.from(header, 'base64');
     const decoded = encoded.toString('ascii');
     const clientPrincipal = JSON.parse(decoded);
-    var rolesArray  = [];
-    if (clientPrincipal.userId == 'd6b9de92ebba49a48e4888c54a3ee8ac' && clientPrincipal.identityProvider == 'google') {
-        rolesArray.push("reader");
-    } else if (clientPrincipal.userId == '1235' && clientPrincipal.identityProvider == 'google') {
-        rolesArray.push("contributor");
-    } else {
-        rolesArray = rolesArray;
+    var rolesArray = [];
+    let id = clientPrincipal.userId;
+    let tok = clientPrincipal.accessToken;
+    let response = await fetch(`https://graph.microsoft.com/v1.0/users/${id}/memberOf`, {
+        method: 'GET',
+        headers: {
+            'Authorization': 'Bearer ' + tok,
+        }
+    });
+    let json = await response.json();
+    console.log(json);
+    for (let x of json.value) {
+        console.log(x.displayName);
+        rolesArray.push(x.displayName);
     }
+    //console.log(response.json);
+    console.log(rolesArray);
 
     context.res.json({
         "userRoles": rolesArray,
         "denyAccess": false
 
     });
-}; 
+};
